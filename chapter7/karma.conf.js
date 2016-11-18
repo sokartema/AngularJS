d55477e1affc531615434b415f994bb277345a3e
Jazer Abreu <sokartema@gmail.com>
2016-11-18 20:47:30 +0000

    Fin del capitulo 7

---
+++ chapter7/karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            '../bower_components/angular/angular.min.js',
+            '../bower_components/angular-mocks/angular-mocks.js',
+            '*.js'
+        ],
+        exclude: [],
+        port: 8080,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+    });
+};
